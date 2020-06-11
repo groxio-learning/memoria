6ca8b6dd429ba412c056d4fcbb7fb1a08440afe3
Richard Miller <millerrichard@google.com>
2020-06-11 18:12:54 -0500

    Stuff and things... OK, we rewote that Counter module thing

--- counter/mix.exs
+++ counter/mix.exs
@@ -5,7 +5,7 @@ defmodule Counter.MixProject do
     [
       app: :counter,
       version: "0.1.0",
-      elixir: "~> 1.10",
+      elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
