8d3773cf3985580054fb16839ed92d0d4f49ded9
Grace McPherson <grace.theprogrammer@gmail.com>
2020-06-18 18:46:25 -0400

    functionality for building our schedule of removal

--- lib/memoria/core.ex
+++ lib/memoria/core.ex
@@ -1,9 +1,26 @@
 defmodule Memoria.Core do
   defstruct [
-    text: "",
-    schedule: [],
+    text:       "",
+    schedule:   [],
   ]
 
+  def new_memoria(text, number_of_steps) do
+    %__MODULE__ {
+    text:       text,
+    schedule:   build_schedule(text, number_of_steps),
+  }
+  end
 
+  def build_schedule(text, number_of_steps) do
+    step_size =
+      text
+      |> String.length
+      |> Kernel./(number_of_steps)
+      |> ceil
+
+    (1..String.length(text))
+    |> Enum.shuffle
+    |> Enum.chunk_every(step_size)
+  end
 
 end
