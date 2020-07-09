090a604819dd7979170e5df4f8903819a320ea6a
mohammed hindawash <djslakivich@gmail.com>
2020-07-09 19:23:05 -0400

    added handle_call function, Linda added init function

--- lib/memoria/boundary.ex
+++ lib/memoria/boundary.ex
@@ -1,3 +1,19 @@
 defmodule Memoria.Boundary do
+  use GenServer
+  alias Memoria.Core
+
+  def init({text, step}) do
+    {:ok, Core.new_memoria(text, step)}
+  end
+
+  def handle_call(:erase, _from, memoria) do
+    next_memoria = Core.erase(memoria)
+    {:reply, next_memoria.text, next_memoria}
+  end
 
 end
+
+
+
+#GenServer.call
+#:sys.get_state
